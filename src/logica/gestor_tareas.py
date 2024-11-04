52621ffc394c865bfca4d5bdaab5a9215f2c0904
Alessandro <lesmoxlol@gmail.com>
2024-11-04 16:53:11 -0500

    Modificaciones

--- src/logica/gestor_tareas.py
+++ src/logica/gestor_tareas.py
@@ -15,16 +15,16 @@ class GestorTareas:
         self.tareas.append(tarea)
     def obtener_tareas(self):
         return self.tareas
-def marcar_completada(self, indice):
-    if 0 <= indice < len(self.tareas):
-        self.tareas[indice].completada = True
-    else:
-        raise IndexError("Índice fuera de rango")
-def eliminar_tarea(self, indice):
-    if 0 <= indice < len(self.tareas):
-        del self.tareas[indice]
-    else:
-        raise IndexError("Índice fuera de rango")
+    def marcar_completada(self, indice):
+        if 0 <= indice < len(self.tareas):
+            self.tareas[indice].completada = True
+        else:
+            raise IndexError("Índice fuera de rango")
+    def eliminar_tarea(self, indice):
+        if 0 <= indice < len(self.tareas):
+            del self.tareas[indice]
+        else:
+            raise IndexError("Índice fuera de rango")
 class GestorTareasGUI:
     def __init__(self, root, gestor):
         self.gestor = gestor
